02a3873a3732af27bfd94f2a9421df16bb897001
Jithin K Jose <jithin.jude68@gmail.com>
2019-03-15 17:46:16 +0530

    humidity and altitude added

---
+++ app/src/main/java/com/zealhack/keralafloodemergencyservicesystem/FloodedRoads.java
@@ -0,0 +1,13 @@
+package com.zealhack.keralafloodemergencyservicesystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FloodedRoads extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flooded_roads);
+    }
+}
--- app/src/main/java/com/zealhack/keralafloodemergencyservicesystem/MainActivity.java
+++ app/src/main/java/com/zealhack/keralafloodemergencyservicesystem/MainActivity.java
@@ -1,16 +1,17 @@
 package com.zealhack.keralafloodemergencyservicesystem;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,6 +19,14 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -27,11 +36,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     LocationTrack locationTrack;
 
     TextView textViewLocation;
+    TextView textViewStatus;
+
+    JSONObject data = null;
+    JSONObject mainObject;
+    String humidity;
+
+    JSONObject dataElavation;
+    JSONArray dataElavationArray;
+    JSONObject dataElavationObject;
+    double elevation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewLocation = (TextView)findViewById(R.id.current_location);
+        textViewStatus = (TextView)findViewById(R.id.flood_alert_status);
 
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
@@ -41,15 +63,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap map) {
-        googleMap = map;
-        if(checkPermission())
-            googleMap.setMyLocationEnabled(true);
-        else askPermission();
-
-        googleMap.setMyLocationEnabled(true);
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(8.5241,76.9366), 10));
-    }
+        googleMap = map;}
 
     private boolean checkPermission() {
         return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -93,26 +107,191 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.option_1:
+                Intent myIntent = new Intent(this, FloodedRoads.class);
+                this.startActivity(myIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void refreshButtonFunction(View view){
-        Toast.makeText(this,"getting location",Toast.LENGTH_SHORT).show();
 
         if(checkPermission())
             googleMap.setMyLocationEnabled(true);
         else askPermission();
 
+        googleMap.setMyLocationEnabled(true);
+
         locationTrack = new LocationTrack(MainActivity.this);
 
 
+        double latitude = locationTrack.getLatitude();
+        double longitude = locationTrack.getLongitude();
 
+        LatLng coordinate = new LatLng(latitude, longitude); //Store these lat lng values somewhere. These should be constant.
+        CameraUpdate location = CameraUpdateFactory.newLatLngZoom(
+                coordinate, 15);
+        googleMap.animateCamera(location);
 
-            double longitude = locationTrack.getLongitude();
-            double latitude = locationTrack.getLatitude();
+        String latitudeString = String.valueOf(latitude);
+        String longitudeString = String.valueOf(longitude);
 
-            String longitudeString = String.valueOf(longitude);
-            String latitudeString = String.valueOf(latitude);
+        getHumidity(latitudeString, longitudeString);
+        getElevation(latitudeString, longitudeString);
 
-        String stringLatitude = String.valueOf(longitudeString+", "+latitudeString);
-        textViewLocation = (TextView)findViewById(R.id.current_location);
+        String stringLatitude = String.valueOf(latitudeString+", "+longitudeString);
         textViewLocation.setText(stringLatitude);
+    }
+
+    public void getHumidity(final String latitudeString, final String longitudeString) {
+
+        new AsyncTask<Void, Void, Void>() {
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    URL url = new URL("http://api.openweathermap.org/data/2.5/weather?lat="+latitudeString+"&lon="+longitudeString+"&APPID=ea574594b9d36ab688642d5fbeab847e");
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    StringBuffer json = new StringBuffer(1024);
+                    String tmp = "";
+
+                    while((tmp = reader.readLine()) != null)
+                        json.append(tmp).append("\n");
+                    reader.close();
+
+                    data = new JSONObject(json.toString());
+
+                    if(data.getInt("cod") != 200) {
+                        System.out.println("Cancelled");
+                        return null;
+                    }
+
+
+                } catch (Exception e) {
+
+                    System.out.println("Exception "+ e.getMessage());
+                    return null;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void Void) {
+                if(data!=null){
+                    Log.d("my weather received",data.toString());
+
+                    try{
+                        mainObject = data.getJSONObject("main");
+                        humidity = mainObject.getString("humidity");
+                    }
+                    catch (Exception e){}
+
+
+
+                    if(checkFloodProbabilityStatus(Integer.parseInt(humidity), elevation)){
+                        textViewStatus.setText("Your location have a humidity of "+humidity+" g/m3 and altitude of "+elevation+" m , which may cause long lasting heavy rainfall. So there may be a probability of flood in your area.");
+                    }else{
+                        textViewStatus.setText("Your location have a humidity of "+humidity+" g/m3 and altitude of "+elevation+" m , there won't be any heavy rainfall . So you are safe from flood.");
+                    }
+                }
+
+            }
+        }.execute();
+    }
+
+    public void getElevation(final String latitudeString, final String longitudeString){
+
+        new AsyncTask<Void, Void, Void>() {
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    URL url = new URL("https://maps.googleapis.com/maps/api/elevation/json?locations="+latitudeString+","+longitudeString+"&key=AIzaSyCpcX-lrVxsuZruEPmSq9dbhBdkB9ccu48");
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    StringBuffer json = new StringBuffer(1024);
+                    String tmp = "";
+
+                    while((tmp = reader.readLine()) != null)
+                        json.append(tmp).append("\n");
+                    reader.close();
+
+                    dataElavation = new JSONObject(json.toString());
+
+                    if(dataElavation.getInt("cod") != 200) {
+                        System.out.println("Cancelled");
+                        return null;
+                    }
+
+
+                } catch (Exception e) {
+
+                    System.out.println("Exception "+ e.getMessage());
+                    return null;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void Void) {
+                if(dataElavation!=null){
+                    Log.d("my altitude JSON",dataElavation.toString());
+
+                    try{
+                        dataElavationArray = dataElavation.getJSONArray("results");
+
+                        dataElavationObject = dataElavationArray.getJSONObject(0);
+                        elevation = dataElavationObject.getDouble("elevation");
+                    }
+                    catch (Exception e){}
+                }
+
+                Log.d("my altitude received",String.valueOf(elevation));
+
+                if(checkFloodProbabilityStatus(Integer.parseInt(humidity), elevation)){
+                    textViewStatus.setText("Your location have a humidity of "+humidity+" g/m3 and altitude of "+elevation+" m , which may cause long lasting heavy rainfall. So there may be a probability of flood in your area.");
+                }else{
+                    textViewStatus.setText("Your location have a humidity of "+humidity+" g/m3 and altitude of "+elevation+" m , there won't be any heavy rainfall . So you are safe from flood.");
+                }
+            }
+        }.execute();
+    }
+
+    public boolean checkFloodProbabilityStatus(int humidity, double elevation){
+        if(humidity >= 80 && elevation < 7){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
